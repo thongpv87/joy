496dd87c85157ff43b661d36e9270252caa5f4df
Thong Pham <thongpv87@gmail.com>
2022-07-20 21:49:11 +0700

    init

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A very basic flake";
+
+  inputs= {
+    haskellNix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux" ] (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          # This overlay adds our project to pkgs
+          hsProject =
+            final.haskell-nix.stackProject' {
+              name = "joy";
+              src = ./.;
+              shell = {
+                tools = {
+                  cabal = {};
+                  haskell-language-server = {};
+                };
+                buildInputs = with pkgs; [ stack protobuf haskellPackages.implicit-hie pkgconfig zlib git haskellPackages.hls-eval-plugin ];
+              };
+            };
+        })
+      ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+        inherit (haskellNix) config;
+      };
+      flake = pkgs.hsProject.flake {};
+    in flake // {
+      # Built by `nix build .`
+      defaultPackage = flake.packages."hello-world";
+    }
+    );
+}
